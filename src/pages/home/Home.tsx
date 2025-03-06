9e945217cfa596bd3010c5c54ed4e321c2f571a8
Adelina_Santos <adevitoria.m09@gmail.com>
2025-03-06 11:23:20 -0300

    Deploy do Projeto Integrador

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -23,7 +23,7 @@ function Home() {
 
                     <div className="flex justify-center ">
                         <img
-                            src="https://i.imgur.com/fyfri1v.png"
+                            src="maps.jpg"
                             alt="Imagem Página Home"
                             className='w-2/3'
                         />
